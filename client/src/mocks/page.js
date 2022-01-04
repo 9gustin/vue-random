0870d4b5ba5c365b96732b950be506ec91a9870f
Agustin Vazquez <agustinvazquez453@gmail.com>
2022-01-04 08:30:28 -0300

    add page mocks

---
+++ client/src/mocks/page.js
@@ -0,0 +1,39 @@
+export const PAGE_MOCK = {
+  object: "page",
+  id: "4742cb3b-f845-4f12-b3cb-4edff2a4f517",
+  created_time: "2022-01-03T20:39:00.000Z",
+  last_edited_time: "2022-01-03T20:39:00.000Z",
+  cover: null,
+  icon: null,
+  parent: {
+    type: "database_id",
+    database_id: "50427144-77bf-4902-9f44-46d45f87bc04",
+  },
+  archived: false,
+  properties: {
+    Tags: { id: "GIJv", type: "multi_select", multi_select: [] },
+    Content: { id: "gDVc", type: "rich_text", rich_text: [] },
+    Column: { id: "owOw", type: "rich_text", rich_text: [] },
+    Name: {
+      id: "title",
+      type: "title",
+      title: [
+        {
+          type: "text",
+          text: { content: "Some React.js Blog", link: null },
+          annotations: {
+            bold: false,
+            italic: false,
+            strikethrough: false,
+            underline: false,
+            code: false,
+            color: "default",
+          },
+          plain_text: "Some React.js Blog",
+          href: null,
+        },
+      ],
+    },
+  },
+  url: "https://www.notion.so/Some-React-js-Blog-4742cb3bf8454f12b3cb4edff2a4f517",
+};

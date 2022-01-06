e9f59a0fa58ec47ef4a291cad015f11649aa0a33
Agustin Vazquez <agustinvazquez453@gmail.com>
2022-01-06 11:52:41 -0300

    detect theme from the navigator

--- client/src/store.js
+++ client/src/store.js
@@ -1,42 +1,46 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 const THEMES = {
-  dark: 'dark',
-  light: 'light'
-}
+  dark: "dark",
+  light: "light",
+};
 
-const THEME_KEY = 'theme'
+const THEME_KEY = "theme";
 
 export default new Vuex.Store({
   state: {
-    theme: localStorage.getItem(THEME_KEY) || THEMES.dark,
+    theme:
+      localStorage.getItem(THEME_KEY) ||
+      window.matchMedia("(prefers-color-scheme: dark)").matches
+        ? THEMES.dark
+        : THEMES.light,
   },
   mutations: {
     SET_DARK_THEME(state) {
-      state.theme = THEMES.dark
-      localStorage.setItem(THEME_KEY, THEMES.dark)
-      document.body.classList.remove('light')
+      state.theme = THEMES.dark;
+      localStorage.setItem(THEME_KEY, THEMES.dark);
+      document.body.classList.remove("light");
     },
     SET_LIGHT_THEME(state) {
-      state.theme = THEMES.light
-      localStorage.setItem(THEME_KEY, THEMES.light)
-      document.body.classList.add('light')
-    }
+      state.theme = THEMES.light;
+      localStorage.setItem(THEME_KEY, THEMES.light);
+      document.body.classList.add("light");
+    },
   },
   actions: {
-    toggleTheme({state, commit}) {
-      if (state.theme === 'dark') {
-        commit('SET_LIGHT_THEME')
+    toggleTheme({ state, commit }) {
+      if (state.theme === "dark") {
+        commit("SET_LIGHT_THEME");
       } else {
-        commit('SET_DARK_THEME')
+        commit("SET_DARK_THEME");
       }
-    }
+    },
   },
   getters: {
-    isDarkTheme: state => state.theme === THEMES.dark,
-    isLightTheme: state => state.theme === THEMES.light
-  }
+    isDarkTheme: (state) => state.theme === THEMES.dark,
+    isLightTheme: (state) => state.theme === THEMES.light,
+  },
 });
